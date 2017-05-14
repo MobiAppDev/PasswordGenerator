b91825b75496f11de7ab7525ed7780b6c6e6e96d
Wayne Hill <waynedhill@live.com>
2017-05-13 22:43:51 -0400

    began adding code to release the keyboard in the Detail View Controller

--- Password Generator/PasswordSettingsDetailViewController/PasswordSettingsDetailViewController.m
+++ Password Generator/PasswordSettingsDetailViewController/PasswordSettingsDetailViewController.m
@@ -6,9 +6,14 @@
 //  Copyright © 2017 Surfside Software Solutions. All rights reserved.
 //
 
+//#import "TouchDelegate.h"
 #import "PasswordSettingsDetailViewController.h"
 
 @interface PasswordSettingsDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *passwordOnDetailPage;
+
+@property (weak, nonatomic) IBOutlet UITextField *savedApplicationNameTextField;
 
 @end
 
@@ -17,12 +22,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
+//  savedApplicationNameTextField.delegate = self;
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(BOOL)textFieldShouldReturn:(UITextField *)savedApplicationNameTextField //textField
+{
+  NSLog(@"Return Key Pressed");
+  [_savedApplicationNameTextField
+resignFirstResponder];
+  return YES;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  NSLog(@"touchBegan");
+//  if (self.delegate && [self.delegate respondsToSelector:@selector(touchesBegan:withEventinView)]) {
+//    [self.delegate touchesBegan:touches withEvent:event inView:self];
+//self.view.endEditing(true);
+}
+
+
+/*
+// For the textView
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+  
+  if([text isEqualToString:@"\n"]) {
+    [textView resignFirstResponder];
+    return NO;
+ }
+  return YES;
+}
+*/
 
 /*
 #pragma mark - Navigation
